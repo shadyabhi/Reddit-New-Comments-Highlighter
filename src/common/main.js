57bd033a8b62c232b9e44a85d8fbc6d70f1dc95c
Wellington Viana <viana.swe@gmail.com>
2018-02-12 16:27:44 -0200

    add support to automatically change theme

--- src/common/main.js
+++ src/common/main.js
@@ -5,6 +5,9 @@ PADDING = 10;
 USE_DARK_THEME = false;
 THEME_DARK_COLOR = '#444444';
 THEME_LIGHT_COLOR = '#FFFDCC';
+AUTO_CHANGE_THEME = false;
+THEME_START_TIME = '18:00';
+THEME_END_TIME = '08:00';
 
 if (kango.storage.getItem("totalitems") === null){
     kango.storage.setItem("totalitems", TOTAL_COMMENTS_TO_REMEMBER);
@@ -31,12 +34,30 @@ if (kango.storage.getItem("themeLightColor") === null){
     console.log("Setting default for light-theme: #%s", THEME_LIGHT_COLOR);
 }
 
+if (kango.storage.getItem("autoChangeTheme") === null){
+    kango.storage.setItem("autoChangeTheme", AUTO_CHANGE_THEME);
+    console.log("Setting default for auto change theme: %s", AUTO_CHANGE_THEME);
+}
+
+if (kango.storage.getItem("themeStartTime") === null){
+    kango.storage.setItem("themeStartTime", THEME_START_TIME);
+    console.log("Setting default for start time of auto change theme: %s", THEME_START_TIME);
+}
+
+if (kango.storage.getItem("themeEndTime") === null){
+    kango.storage.setItem("themeEndTime", THEME_END_TIME);
+    console.log("Setting default for end time of auto change theme: %s", THEME_END_TIME);
+}
+
 function getSettings() {
     return {
         'totalitems': kango.storage.getItem('totalitems'),
         'useDarkTheme': kango.storage.getItem('useDarkTheme'),
         'themeDarkColor': kango.storage.getItem('themeDarkColor'),
         'themeLightColor': kango.storage.getItem('themeLightColor'),
-        "padding": kango.storage.getItem('padding')
+        "padding": kango.storage.getItem('padding'),
+        'autoChangeTheme': kango.storage.getItem('autoChangeTheme'),
+        'themeStartTime': kango.storage.getItem('themeStartTime'),
+        'themeEndTime': kango.storage.getItem('themeEndTime')
     };
 }
--- src/common/options.js
+++ src/common/options.js
@@ -7,6 +7,9 @@ function save_options() {
     var useDarkTheme = document.getElementById("theme");
     var darkTheme = document.getElementById("theme-dark");
     var lightTheme = document.getElementById("theme-light");
+    var autoChangeTheme = document.getElementById("auto-change-theme");
+    var themeStartTime = document.getElementById("theme-start-time");
+    var themeEndTime = document.getElementById("theme-end-time");
 
     localStorage["totalitems"] = totalitems_input.value;
     localStorage["padding"] = padding_input.value;
@@ -14,6 +17,9 @@ function save_options() {
     localStorage["themeDarkColor"] = JSON.stringify(darkTheme.value);
     localStorage["themeLightColor"] = JSON.stringify(lightTheme.value);
     localStorage["useDarkTheme"] = useDarkTheme.checked;
+    localStorage["autoChangeTheme"] = autoChangeTheme.checked;
+    localStorage["themeStartTime"] = '"'+themeStartTime.value+'"';
+    localStorage["themeEndTime"] = '"'+themeEndTime.value+'"';
 }
 
 // Restores select box state to saved value from localStorage.
@@ -24,12 +30,18 @@ function restore_options() {
     var useDarkTheme = document.getElementById("theme");
     var darkTheme = document.getElementById("theme-dark");
     var lightTheme = document.getElementById("theme-light");
+    var autoChangeTheme = document.getElementById("auto-change-theme");
+    var themeStartTime = document.getElementById("theme-start-time");
+    var themeEndTime = document.getElementById("theme-end-time");
 
     totalitems_input.value = localStorage["totalitems"];
     padding_input.value = localStorage["padding"];
     darkTheme.value = JSON.parse(localStorage["themeDarkColor"]);
     lightTheme.value = JSON.parse(localStorage["themeLightColor"]);
     useDarkTheme.checked = localStorage["useDarkTheme"] === "true";
+    autoChangeTheme.checked = localStorage["autoChangeTheme"] === "true";
+    themeStartTime.value = localStorage["themeStartTime"].replace(/\"|\\/g, '');
+    themeEndTime.value = localStorage["themeEndTime"].replace(/\"|\\/g, '');
 }
 
 //Do all this stuff when page loads
@@ -37,10 +49,23 @@ window.onload = function () {
     restore_options();
     var button = document.getElementsByTagName("button")[0];
 
-
     button.addEventListener( 'click', function() {
         save_options();
         button.className  = button.className + " btn-success";
         button.innerText = "Success. Settings saved";
     }, false);
-}
+    
+    var disableTimeBoxes = function() {
+        if(!autoChangeCheckbox.checked) {
+            document.getElementById("theme-start-time").disabled = true;
+            document.getElementById("theme-end-time").disabled = true;
+        }
+        else {
+            document.getElementById("theme-start-time").disabled = false;
+            document.getElementById("theme-end-time").disabled = false;
+        }
+    };
+    var autoChangeCheckbox = document.getElementById("auto-change-theme");
+    autoChangeCheckbox.addEventListener( 'change', disableTimeBoxes);
+    disableTimeBoxes();
+};
